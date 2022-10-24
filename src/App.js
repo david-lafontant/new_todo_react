8198f72b845d105c616ffe1034ff6edd1aec27f9
david-lafontant <lafontant.david@gmail.com>
2022-10-24 17:24:55 -0600

    Import and display task component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header/header';
+import Task from './components/task/task';
 
 function App() {
   return (
     <div className="App">
       <Header title="To Do react" />
+      <Task />
     </div>
   );
 }
