d8f6db1083b7530c518097885f3075b2fbb9a680
david-lafontant <lafontant.david@gmail.com>
2022-10-24 11:36:20 -0600

    Fix error in header import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Header } from './components/header/header'
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
-    <Header />  
+      <Header />
     </div>
   );
 }
