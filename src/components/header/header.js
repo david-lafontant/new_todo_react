72da2c8a449fc3ea6fdd1c7517aa9ab18338d203
david-lafontant <lafontant.david@gmail.com>
2022-10-24 16:17:22 -0600

    Display title props

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,18 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Button from '../button/button';
 
-function Header(props) {
+function Header({ title }) {
   return (
     <div>
-      <h1>{props.title}</h1>
+      <h1>{title}</h1>
       <Button />
     </div>
   );
 }
 
-
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default Header;
