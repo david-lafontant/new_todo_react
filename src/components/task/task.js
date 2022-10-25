179db0c137a44658e91a6ebc953e574609d9b6c2
david-lafontant <lafontant.david@gmail.com>
2022-10-24 19:01:43 -0600

    Pass task props to TaskItem

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import TaskItem from '../task-item/task-item';
 
 function Task() {
+  const tasks = [
+    {
+      id: 1,
+      text: 'task 1',
+      date: 'May 16, 2022',
+    },
+    {
+      id: 2,
+      text: 'task 2',
+      date: 'May 17, 2022',
+    },
+    {
+      id: 3,
+      text: 'task 3',
+      date: 'May 18, 2022',
+    },
+  ];
+
   return (
-    <div>Task</div>
+    <div>
+      <ul>
+        {tasks.map((task) => (<TaskItem key={task.id} task={task} />))}
+
+      </ul>
+
+    </div>
   );
 }
 
